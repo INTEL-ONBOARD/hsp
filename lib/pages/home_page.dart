aee32faa35984160ee4125dedde8b8a9fb5438ac
INTEL-ONBOARD <75471327+INTEL-ONBOARD@users.noreply.github.com>
2024-11-23 01:27:47 +0530

    color fixes for body section

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,6 +22,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
           title: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
