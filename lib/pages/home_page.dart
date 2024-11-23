bb69c0548fddd5e35f0b13fbe0af7e347e9829fd
B1N4L <163591915+B1N4L@users.noreply.github.com>
2024-11-23 23:02:28 +0530

    update home_page.dart: successScreen stateless widget was added.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -133,6 +133,62 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+//widget for success/fail message screens.
+class SuccessScreen extends StatelessWidget {
+  final String imagePath;
+  final String title;
+  final String message;
+
+  const SuccessScreen({
+    Key? key,
+    required this.title,
+    required this.imagePath,
+    required this.message,
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //image file path
+              Image.asset(imagePath, width: 100, height: 100,),
+
+              Text(
+                title, style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              //const SizedBox(height: 24,),
+              Text(
+                message, style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black),
+              ),
+              const SizedBox(height: 14,),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: const Color.fromRGBO(94, 105, 207, 1),
+                  backgroundColor: Colors.white,
+                  side: const BorderSide(color: Color.fromRGBO(94, 105, 207, 1), width: 1),
+                  minimumSize: const Size(160, 45), // Button size
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                onPressed: () {
+                  // Handle "Done" button action
+                },
+                child: const Text('Done', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400,),),
+              ),
+            ],
+          )
+      ),
+    );
+  }
+}
+
+
 class MarketPlacePage extends StatelessWidget {
   const MarketPlacePage({super.key});
 
